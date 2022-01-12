69ed541667d2075cb88e158f211b43084c4b978a
Moureen <moureenham@gmail.com>
2022-01-13 00:33:19 +0530

    loader created

---
+++ src/common/Loader/AddverbLoader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./addverbLoader.css";
+
+export const AddverbLoader = () => {
+  return (
+    <div className="loader">
+      {" "}
+      <span>Loading... Please wait</span>
+    </div>
+  );
+};
